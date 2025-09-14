f15b6fa5013b8ef1431247cac12b31b4975d66a0
Heming Harnaes <heming.harnaes@gmail.com>
2025-09-14 22:47:01 +0200

    #7 Blob operations

--- integration_service/adapters/google_cloud_storage_adapter.py
+++ integration_service/adapters/google_cloud_storage_adapter.py
@@ -34,3 +34,58 @@ class GoogleCloudStorageAdapter:
         return (
             f"{storage_server}/{storage_bucket}/{destination_blob_name}"
         )
+
+    def move_blob(self, source_blob_name: str, destination_blob_name: str) -> str:
+        """Move a blob within the bucket, return URL to moved file."""
+        servicename = "GoogleCloudStorageAdapter.move_blob"
+        storage_bucket = os.getenv("GOOGLE_STORAGE_BUCKET", "")
+        storage_server = os.getenv("GOOGLE_STORAGE_SERVER", "")
+        if storage_bucket == "" or storage_server == "":
+            err_msg = "GOOGLE_STORAGE_BUCKET or GOOGLE_STORAGE_SERVER not found in .env"
+            raise Exception(err_msg)
+
+        try:
+            storage_client = storage.Client()
+            bucket = storage_client.bucket(storage_bucket)
+            blob = bucket.blob(source_blob_name)
+            new_blob = bucket.rename_blob(blob, destination_blob_name)
+        except Exception as e:
+            logging.exception(servicename)
+            raise Exception(servicename) from e
+        return (
+            f"{storage_server}/{storage_bucket}/{new_blob.name}"
+        )
+
+    def list_blobs(self, prefix: str) -> list[str]:
+        """List all blobs in the bucket that begin with the prefix."""
+        servicename = "GoogleCloudStorageAdapter.get_blobs"
+        storage_bucket = os.getenv("GOOGLE_STORAGE_BUCKET", "")
+        if storage_bucket == "":
+            err_msg = "GOOGLE_STORAGE_BUCKET not found in .env"
+            raise Exception(err_msg)
+
+        try:
+            storage_client = storage.Client()
+            bucket = storage_client.bucket(storage_bucket)
+            blobs = bucket.list_blobs(prefix=prefix)
+            return [blob.name for blob in blobs]
+        except Exception as e:
+            logging.exception(servicename)
+            raise Exception(servicename) from e
+
+    def delete_blob(self, blob_name: str) -> None:
+        """Delete a blob in the bucket."""
+        servicename = "GoogleCloudStorageAdapter.delete_blob"
+        storage_bucket = os.getenv("GOOGLE_STORAGE_BUCKET", "")
+        if storage_bucket == "":
+            err_msg = "GOOGLE_STORAGE_BUCKET not found in .env"
+            raise Exception(err_msg)
+
+        try:
+            storage_client = storage.Client()
+            bucket = storage_client.bucket(storage_bucket)
+            blob = bucket.blob(blob_name)
+            blob.delete()
+        except Exception as e:
+            logging.exception(servicename)
+            raise Exception(servicename) from e
